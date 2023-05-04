a594ed071f245e694882ea999381e331f12ee9ee
Joseph Warner <josephtoko77@gmail.com>
2023-05-04 16:01:52 +0100

    Settings Menu Works
    Full settings menu functionality

---
+++ Assets/SettingsMenu.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+public class SettingsMenu : MonoBehaviour
+{
+  public AudioMixer audioMixer;
+  public TMPro.TMP_Dropdown resolutionDropdown;
+  Resolution[] resolutions;
+
+  private void Start()
+  {
+    resolutions = Screen.resolutions;
+    resolutionDropdown.ClearOptions();
+    List<string> options = new List<string>();
+    int currentResolutionIndex = 0;
+    for (int i = 0; i < resolutions.Length; i++)
+    {
+      string option = resolutions[i].width + " x " + resolutions[i].height;
+      options.Add(option);
+      if (resolutions[i].width == Screen.currentResolution.width &&
+          resolutions[i].height == Screen.currentResolution.height)
+      {
+        currentResolutionIndex = i;
+      }
+    }
+    resolutionDropdown.AddOptions(options);
+    resolutionDropdown.value = currentResolutionIndex;
+    resolutionDropdown.RefreshShownValue();
+  }
+
+  public void SetVolume(float volume)
+  {
+    audioMixer.SetFloat("volume", volume);
+  }
+
+  public void SetFullscreen(bool isFullscreen)
+  {
+    Screen.fullScreen = isFullscreen;
+  }
+}
